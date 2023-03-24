1aca12f1c0de72e790d9ef8480e51de053227a83
juankalaprint <105978502+juankalaprint@users.noreply.github.com>
2023-03-24 11:30:58 -0500

    Add files via upload

--- Ejercicio 01.js
+++ Ejercicio 01.js
@@ -1,37 +1,53 @@
 /*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
 /*1️⃣ EJERCICIO 01 1️⃣*/
 
-// En los siguientes ejercicios deberás reemplazar el valor null
-// por el correspondiente.
+function crearUsuario() {
+   // Crea una Clase de ES6 o una función constructora llamada "Usuario".
+   // El constructor de esta clase debe tener las propiedades: "usuario", "nombre", "email" y "password".
+   // El valor de cada propiedad la recibirá por parámetro.
+   // Además, esta clase debe tener un método llamado "saludar".
+   // Este método debe retornar el string: 'Hola, mi nombre es {{nombre}}'.
+   // El valor {{nombre}} debe ser el nombre definido para la instancia de la clase.
+   // Retornar la clase.
+   // Tu código:
 
-// Crea una variable de tipo string.
-const nuevoString = "null";
+   function Usuario(usuario, nombre, email, password) {
+      this.usuario = usuario;
+      this.nombre = nombre;
+      this.email = email;
+      this.password = password;
+   }
+   Usuario.prototype.saludar = function() {
+      return 'Hola, mi nombre es ' + this.nombre;
+   };
+   return Usuario;
+}
 
-// Crea una variable de tipo number.
-const nuevoNumero = 3;
+function agregarMetodoPrototype(Usuario) {
+   // Agrega un método al prototipo de "Usuario".
+   // El método debe llamarse "saludar" y debe devolver el string "Hello World!".
+   // Tu código:
+   Usuario.prototype.saludar = function() {
+      return "Hello World!";
+   };
+}
 
-// Crea una variable de tipo boolean.
-const nuevoBoolean = False;
-
-// Resuelve el siguiente problema matemático.
-const nuevaResta = 10 - 7 === 3;
-
-// Resuelve el siguiente problema matemático.
-const nuevaMultiplicacion = 10 * 4 === 40;
-
-// Resuelve el siguiente problema matemático.
-const nuevoModulo = 21 % 5===1;
+function agregarStringInvertida() {
+   // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
+   // El método debe llamarse "reverse".
+   // [PISTA]: necesitarás utilizar el objeto "this".
+   String.prototype.reverse = function() {
+      var stringInvertida = '';
+      for(var i = this.length - 1; i>=0; i--) {
+         stringInvertida = stringInvertida + this.charAt(i);
+      }
+      return stringInvertida;
+   };
+}
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
-   nuevoString,
-   nuevoNumero,
-   nuevoBoolean,
-   nuevaResta,
-   nuevaMultiplicacion,
-   nuevoModulo,
+   crearUsuario,
+   agregarMetodoPrototype,
+   agregarStringInvertida,
 };
-function newFunction() {
-   return 14;
-}
-
--- Ejercicio 02.js
+++ Ejercicio 02.js
@@ -1,57 +1,48 @@
 /*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
 /*2️⃣ EJERCICIO 02 2️⃣*/
 
-function devolverString(string) {
-   // Debe retornar un string.
-   // Tu código:
-   return (string);
-}
+class Persona {
+   // Crea el constructor de la clase "Persona".
+   // Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio".
+   // Debe tener un método llamado "detalle" que nos devuelva un objeto con las propiedades de la persona y
+   // sus valores.
 
-// ⛔️ "X" e "Y" son números.
+   constructor(nombre, apellido, edad, domicilio) {
+      this.nombre = nombre,
+      this.apellido = apellido,
+      this.edad = edad,
+      this.domicilio = domiciliothis.detalle = function() {
+         return {nombre: this.nombre,apellido: this.apellido,edad: this.edad,domicilio: this.domicilio}
+      }
+   }
 
-function suma(x, y) {
-   // Retorna el resultado de su suma.
-   // Tu código:
-
-return x + y;
-  
-}
-
-function resta(x, y) {
-   // Retorna el resultado de la resta.
-   // Tu código:
-   
-   return x - y;
 
 }
 
-function divide(x, y) {
-   // Retorna el resultado de su división.
+function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
+   // En este ejercicio debes crear una instancia de la clase construida en el ejercicio anterior.
+   // Recibirás las propiedades por parámetro.
+   // Retornar la instancia creada.
    // Tu código:
-   
-   return x / y;
+
+   var persona = new Persona (nombre, apellido, edad, domicilio);
+   return persona;
 }
 
-function multiplica(x, y) {
-   // Retorna el resultado de su multiplicación.
+function agregarMetodo() {
+   // La función agrega un método "datos" a la clase "Persona".
+   // Este método toma la propiedad "nombre" y "edad", y devuelve el string:
+   // Ejemplo: "Juan, 22 años".
    // Tu código:
-   ;
-   return x * y;
-}
+   Persona.prototype.datos = function(){
+      return this.nombre + ', ' + this.edad + ' años';
+   }
 
-function obtenerResto(x, y) {
-   // Obten el resto de la división de "x" entre "y".
-   // Tu código:
-   ;
-   return x % y;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
-   devolverString,
-   suma,
-   resta,
-   divide,
-   multiplica,
-   obtenerResto,
+   crearInstanciaPersona,
+   agregarMetodo,
+   Persona,
 };
